4aaeddcfa3c9e6eb6ecd837698d2aee1992b51e5
peterdinis <petter1@pobox.sk>
2025-01-03 00:07:26 +0100

    feat: Create profile dropdown component

---
+++ src/app/_components/auth/ProfileDropdown.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const ProfileDropdown: FC = () => {
+    return (
+        <>
+         Profile Dropdown
+        </>
+    )
+}
+
+export default ProfileDropdown
--- src/app/_components/shared/Navigation.tsx
+++ src/app/_components/shared/Navigation.tsx
@@ -1,20 +1,24 @@
-"use client";
-
 import { FC } from "react";
 import { Avatar, AvatarFallback } from "~/components/ui/avatar";
 import ModeToggle from "./ModeToggle";
+import ProfileDropdown from "../auth/ProfileDropdown";
 
 const Navigation: FC = () => {
   return (
-    <header className="mx-auto flex w-full max-w-screen-2xl justify-between bg-transparent px-10 py-10">
-      <div className="flex items-center gap-2">
-        <Avatar>
-          <AvatarFallback>AI</AvatarFallback>
-        </Avatar>
-        <span>Avatars</span>
-      </div>
-      <ModeToggle />
-    </header>
+    <>
+      <header className="mx-auto flex w-full max-w-screen-2xl justify-between bg-transparent px-10 py-10">
+        <div className="flex items-center gap-2">
+          <Avatar>
+            <AvatarFallback>AI</AvatarFallback>
+          </Avatar>
+          <span>Avatars</span>
+        </div>
+        <div className="flex items-center gap-4">
+          <ModeToggle />
+          <ProfileDropdown />
+        </div>
+      </header>
+    </>
   );
 };
 
