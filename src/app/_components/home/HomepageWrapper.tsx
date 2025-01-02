749265defa5fec2adc8b06f9b2ba99f9c66d5a3c
peterdinis <petter1@pobox.sk>
2025-01-02 12:00:24 +0100

    feat: Working on auth

--- src/app/_components/home/HomepageWrapper.tsx
+++ src/app/_components/home/HomepageWrapper.tsx
@@ -1,9 +1,13 @@
-import { FC } from "react";
+"use client"
+
+import { FC, useState } from "react";
 import { Button } from "~/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "~/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "~/components/ui/dialog";
 import { Input } from "~/components/ui/input";
+import { FaGoogle } from "react-icons/fa";
 
 const HomepageWrapper: FC = () => {
+    const [email, setEmail] = useState("");
     return (
         <div className="flex h-full w-full flex-col items-center justify-center">
             <div className="fixed inset-0 -z-10 bg-gradient-to-br from-fuchsia-200 via-slate-50 to-red-300"></div>
@@ -34,10 +38,14 @@ const HomepageWrapper: FC = () => {
                                     <DialogTitle>Complete authentification</DialogTitle>
                                     <DialogDescription>Two authentification flows are here</DialogDescription>
                                 </DialogHeader>
-                                <form className="flex flex-col space-y-4">
-                                    <Input type={"text"} />
+                                <form onSubmit={(e) => e.preventDefault()} className="flex flex-col space-y-4">
+                                    <Input value={email} onChange={(e) => setEmail(e.target.value)} type={"email"} required placeholder="john@doe.com" />
                                     <Button type="submit" className="w-full">Verify your email</Button>
                                 </form>
+                                <p className="w-full text-center font-bold">OR</p>
+                                <Button className="mr-2" variant={"secondary"} size={"sm"}>
+                                    <FaGoogle /> Login
+                                </Button>
                             </DialogContent>
                         </Dialog>
                     </div>
