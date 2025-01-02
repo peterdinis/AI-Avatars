4a374f4ffa74d3cd32d36b13dff6d6cb5930db50
peterdinis <petter1@pobox.sk>
2025-01-02 14:27:42 +0100

    feat: Delete clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,7 @@ import "~/styles/globals.css";
 import { type Metadata } from "next";
 import { TRPCReactProvider } from "~/trpc/react";
 import { Toaster } from "~/components/ui/toaster";
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
+
 export const metadata: Metadata = {
   title: "AI Avatars",
   description: "Generate your ai avatars here",
@@ -18,10 +16,8 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <TRPCReactProvider>
-          <ClerkProvider>
             {children}
             <Toaster />
-          </ClerkProvider>
         </TRPCReactProvider>
       </body>
     </html>
