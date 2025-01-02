e2ae932e5e274d9d1f7e4b148140ad16b9f98b1e
peterdinis <petter1@pobox.sk>
2025-01-03 00:14:54 +0100

    feat: Install new shadcn ui components

--- src/app/_components/auth/ProfileDropdown.tsx
+++ src/app/_components/auth/ProfileDropdown.tsx
@@ -1,10 +1,36 @@
+import Link from "next/link";
 import { FC } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { Button } from "~/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "~/components/ui/dropdown-menu";
 
 const ProfileDropdown: FC = () => {
     return (
-        <>
-         Profile Dropdown
-        </>
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>JD</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>John Doe</DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Button size={"sm"} variant={"link"}>
+                        <Link href={"/billing"}>
+                            Billing
+                        </Link>
+                    </Button>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Button size={"sm"} variant={"link"}>
+                        Logout
+                    </Button>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
 
