2d472053d2c277390e4af8cc668274c87fd08374
peterdinis <petter1@pobox.sk>
2025-01-02 10:57:13 +0100

    infra: Working on landing page

--- src/app/_components/home/HomepageWrapper.tsx
+++ src/app/_components/home/HomepageWrapper.tsx
@@ -4,6 +4,14 @@ import { Button } from "~/components/ui/button";
 const HomepageWrapper: FC = () => {
   return (
     <div className="flex h-full w-full items-center justify-center">
+      <header className="flex w-full justify-between p-4 px-10">
+        <div>
+            LOGO
+        </div>
+        <div>
+            MENU
+        </div>
+      </header>
       <div className="flex flex-col space-y-4">
         <h1 className="text-3xl font-bold">Generate AI Avatars</h1>
         <Button variant={"default"} size={"lg"}>
